03d5509f09dca59ec8d0acc82ff275f4c13845a3
SnowDrive <luc.moralesr@gmail.com>
2023-08-12 07:12:16 -0400

    test 8

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -9,4 +9,4 @@ export { InvalidUserError, InvalidUserMessages } from 'domain/user/errors/invali
 // Package entry point
 export { AfordiBot } from 'src/afordibot'
 
-console.log('test-ci-9')
+console.log('test-ci-10')
